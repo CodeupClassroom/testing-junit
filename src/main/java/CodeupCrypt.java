8f8c1dd2d4bd5f6d384146816d2a303788ba3de9
Fernando Mendoza <fernandoza@gmail.com>
2019-10-07 15:44:05 -0500

    feat: create a TDD example by testing code before the implementation

--- src/main/java/CodeupCrypt.java
+++ src/main/java/CodeupCrypt.java
@@ -1,8 +1,39 @@
 public class CodeupCrypt {
     private static String hash = "";
 
-    public static String getHash() {
+    public static String getHash(String originalStr) {
         // real procedure
-        return "F3r";
+        hash = "";
+        for (char character : originalStr.toCharArray()) {
+            switch (character){
+                case 'a':
+                case 'A':
+                    hash += 4;
+                    break;
+                case 'e':
+                case 'E':
+                    hash += 3;
+                    break;
+                case 'i':
+                case 'I':
+                    hash += 1;
+                    break;
+                case 'o':
+                case 'O':
+                    hash += 0;
+                    break;
+                case 'u':
+                case 'U':
+                    hash += 9;
+                    break;
+                default:
+                    hash += character;
+            }
+        }
+        return hash;
+    }
+
+    public static boolean checkPassword(String password) {
+        return getHash(password).equals(password);
     }
 }
--- src/test/java/CodeupCryptTest.java
+++ src/test/java/CodeupCryptTest.java
@@ -5,9 +5,21 @@ public class CodeupCryptTest {
 
     @Test
     public void testIfHashExists(){
-        assertNotNull(CodeupCrypt.getHash());
-        assertEquals( "F3r", CodeupCrypt.getHash());
-        assertEquals( "n4nd0", CodeupCrypt.getHash());
+        assertNotNull(CodeupCrypt.getHash(""));
+    }
+
+    @Test
+    public void testIfHashWorks(){
+        assertEquals( "", CodeupCrypt.getHash(""));
+        assertEquals( "F3r", CodeupCrypt.getHash("Fer"));
+        assertEquals( "n4nd0", CodeupCrypt.getHash("nando"));
+        assertEquals("43109", CodeupCrypt.getHash("aeiou"));
+    }
+
+    @Test
+    public void testIfCheckPasswordWorks(){
+        assertTrue(CodeupCrypt.checkPassword("F3r"));
+        assertFalse(CodeupCrypt.checkPassword("n4ndo"));
     }
 
 }
