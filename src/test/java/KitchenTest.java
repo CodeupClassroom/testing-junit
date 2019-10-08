047bbc07bdd745850301703b9c8e504409ee1589
Fernando Mendoza <fernandoza@gmail.com>
2019-10-08 12:31:24 -0500

    feat: finish a couple of tests for existing code

--- src/test/java/KitchenTest.java
+++ src/test/java/KitchenTest.java
@@ -14,26 +14,42 @@ public class KitchenTest {
         emptyKitchen = new Kitchen();
 
         kitchenWithOne = new Kitchen();
-//        kitchenWithOne.add("red tortilla");
+        kitchenWithOne.add("red tortilla");
+
+        kitchenWithMany = new Kitchen();
+        kitchenWithMany.add("blue tortilla");
+        kitchenWithMany.add("red tortilla");
+        kitchenWithMany.add("yellow tortilla");
     }
 
     @Test
     public void testIfTortillaExists(){
-        assertEquals(0, kitchenWithOne.size());
-        assertTrue(kitchenWithOne.isEmpty());
-
-        kitchenWithOne.add("blue tortilla");
+        assertEquals(0, emptyKitchen.size());
+        assertTrue(emptyKitchen.isEmpty());
 
         assertEquals(1, kitchenWithOne.size());
         assertFalse(kitchenWithOne.isEmpty());
+
+        assertEquals(3, kitchenWithMany.size());
+        assertFalse(kitchenWithMany.isEmpty());
     }
 
 //    Integration tests 1/2
     @Test
     public void testIfATortillaCanBeAdded(){
-        assertFalse(kitchenWithOne.contains("red tortilla"));
-        kitchenWithOne.add("red tortilla");
+        assertFalse(emptyKitchen.contains("red tortilla"));
         assertTrue(kitchenWithOne.contains("red tortilla"));
+        assertTrue(kitchenWithMany.contains("red tortilla"));
+    }
+
+
+    @Test
+    public void testIfTortillaCanBePopped(){
+        kitchenWithOne.pop();
+        assertEquals(0, kitchenWithOne.size());
+
+        kitchenWithMany.pop();
+        assertEquals(2, kitchenWithMany.size());
     }
 
 
